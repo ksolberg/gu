55f31e53d91daf9f0284d68289a1e3ca1338b829
Kenneth Solberg <kenneth.solberg@novacare.no>
2023-04-25 19:14:02 +0200

    initial commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        gold: "#E9E8E3",
+				gold2: "#8F8A72"
+      },
+    },
+  },
+  plugins: [],
+};
