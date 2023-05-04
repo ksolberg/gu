ef2dbc212f12fe672a4cc4d7f989520833ddfd0b
Kenneth Solberg <kenneth.solberg@novacare.no>
2023-05-04 15:15:20 +0200

    font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
+    fontFamily: {
+      sans: ["sofia-pro", "sans-serif"]
+    },
+    fontWeight: {
+      light: '300',
+      normal: '400',
+      medium: '500'
+    },
     extend: {
       colors: {
         gold: "#E9E8E3",
